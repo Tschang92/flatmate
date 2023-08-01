f69174f51002cf8fbdd373f81406eb100189bd65
Gianni <gianluca92.caputo@gmail.com>
2023-08-01 16:45:13 +0200

    Adds User registration and login functionality

--- flatmate/base/urls.py
+++ flatmate/base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import ShoppingList, ShoppingDetail, ShoppingCreate, ShoppingUpdate, DeleteView
+from .views import ShoppingList, ShoppingDetail, ShoppingCreate, ShoppingUpdate, DeleteView, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', ShoppingList.as_view(), name='shoppingList'),
     path('shoppingItem/<int:pk>', ShoppingDetail.as_view(), name='shoppingItem'),
     path('shoppingItem-create/', ShoppingCreate.as_view(), name='shoppingItem-create'),
--- flatmate/base/views.py
+++ flatmate/base/views.py
@@ -1,34 +1,78 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from .models import ShoppingItem
 
 
 # Create your views here.
-class ShoppingList(ListView):
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('shoppingList')
+
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('shoppingList')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('shoppingList')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+class ShoppingList(LoginRequiredMixin, ListView):
     model = ShoppingItem
     context_object_name = 'shoppingList'
     template_name = 'base/shoppinglist.html'
 
+    # Prevent users from seeing shopping items of other users
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['shoppingList'] = context['shoppingList'].filter(user=self.request.user)
+        context['count'] = context['shoppingList'].filter(complete=False).count()
+        return context
 
-class ShoppingDetail(DetailView):
+
+class ShoppingDetail(LoginRequiredMixin, DetailView):
     model = ShoppingItem
     context_object_name = 'shoppingItem'
     template_name = 'base/shoppingitem.html'
 
-class ShoppingCreate(CreateView):
+class ShoppingCreate(LoginRequiredMixin, CreateView):
     model = ShoppingItem
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('shoppingList')
 
-class ShoppingUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(ShoppingCreate, self).form_valid(form)
+
+class ShoppingUpdate(LoginRequiredMixin, UpdateView):
     model = ShoppingItem
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('shoppingList')
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = ShoppingItem
     context_object_name = 'shoppingItem'
     success_url = reverse_lazy('shoppingList')
