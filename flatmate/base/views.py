5482f99c99368ffb479699fde3f8275e4f64b89d
Gianni <gianluca92.caputo@gmail.com>
2023-08-06 17:40:58 +0200

    Adds search functionality and form in list view

--- flatmate/base/views.py
+++ flatmate/base/views.py
@@ -50,6 +50,13 @@ class ShoppingList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['shoppingList'] = context['shoppingList'].filter(user=self.request.user)
         context['count'] = context['shoppingList'].filter(complete=False).count()
+
+        search_input = self.request.GET.get("search-area") or ''
+        if search_input:
+            context['shoppingList'] = context['shoppingList'].filter(title__icontains=search_input)
+
+        context['search_input'] = search_input
+
         return context
 
 
